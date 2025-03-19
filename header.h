03c370ea21eed322272befa30484e2cc917e0ca2
daiguanzi <3220103437@zju.edu.cn>
2025-03-19 15:40:20 +0800

    failed

--- header.h
+++ header.h
@@ -4,7 +4,7 @@
 
 #define DA_CH1 XBYTE[0x5fff] // CS1，向DA1输出011
 #define DA_CH2 XBYTE[0x3fff] // CS2，向DA2输出001
-#define GEN_LEN 400          // 生成指定波形的长度
+#define GEN_LEN 200          // 生成指定波形的长度
 #define BASE_LINE 128        // 基线
 #define AD_LEN 250           // 幅值，频率测量时用,采样一次用1ms，所以250次采样就是0.25s（因为ppt要求刷新率不低于4Hz）
 #define DA_LEN 8192          // 存储DA数据的内存长度
